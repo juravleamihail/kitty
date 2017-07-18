2ff278118f1c04b7110cf1e5082f14cd999d767a
iquest <iquest@SB070020PC>
2017-07-18 15:03:36 +0300

    change

---
+++ Kitty/Bank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bank
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public void TransferFunds()
+        {
+            Account source = new Account();
+            source.Deposit(200m);
+            Assert.AreEqual(200m, source.Balance);
+
+            Account destination = new Account();
+            destination.Deposit(250m);
+
+            Assert.AreEqual(250m, destination.Balance);
+
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(350m, destination.Balance);
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    
+}
