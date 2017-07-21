81c7baf3febd25be2f74fb44f3cf5e29467bf455
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-21 11:23:01 +0300

    ion

--- Kitty/Tools/EmailService.cs
+++ Kitty/Tools/EmailService.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace Kitty.Tools
 {
     public class EmailService: IEmailService
     {
+        Email email;
+
+        string smtpAddress = "smtp.gmail.com";
+        int portNumber = 587;
+        bool enableSSL = true;
+
+
+
+
         public void Send(Email email)
         {
+            this.email = email;
+
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(email.From);
+                mail.To.Add(email.To);
+                mail.Subject = email.Subject;
+                mail.Body = email.Body;
+                mail.IsBodyHtml = false;
+                // Can set to false, if you are sending pure text.
+
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    //smtp.Credentials = new NetworkCredential(email.From, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+
+
+            }
 
         }
-
-    }
 }
