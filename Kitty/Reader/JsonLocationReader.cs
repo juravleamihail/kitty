66c0b45a674011e48b2ebae6618abc1eaaaa2315
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-31 15:07:25 +0300

    asd

---
+++ Kitty/Reader/JsonLocationReader.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Kitty.Reader
+{
+    public static class JsonLocationReader
+    {
+        //private static List<Location> locations;
+
+        public static string ReadJsonFile()
+
+        {
+
+            string json = string.Empty;
+
+            using (StreamReader r = new StreamReader("C:\\Users\\iquest\\Source\\Repos\\kitty\\kitty\\Kitty\\countriesToCities.json"))
+
+            {
+
+                json = r.ReadToEnd();
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+            }
+
+            return json;
+
+        }
+
+        public static string[] SplitCities(string input)
+        {
+            string[] result = null;
+
+            char[] delimiterChars = { ' ', '"', '{', '}', ',', '/', '\\', '.', ':', '[', ']', '\t' };
+            result = input.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+          
+
+            return result;
+        }
+    }
+}
---
+++ Tests/ReadingFromFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kitty.Reader;
+using Kitty;
+using System.Data.Entity;
+
+
+namespace Tests
+{
+    [TestClass]
+   public class ReadingFromFile
+    {
+
+        [TestMethod]
+        public void CanReadFromJsonFile()
+        {
+            string locations = null;
+            locations = JsonLocationReader.ReadJsonFile();
+            Assert.IsNotNull(locations);
+        }
+
+        [TestMethod]
+        public void CanSplitStrigInCities()
+        {
+            string locations = JsonLocationReader.ReadJsonFile();
+            string[] cities = JsonLocationReader.SplitCities(locations);
+            Assert.IsNotNull(cities);
+
+        }
+
+    }
+}
