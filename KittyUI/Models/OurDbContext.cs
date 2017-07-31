01fa2e26857490b2a95450c0dbdd2b8a4d160656
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-31 10:47:26 +0300

    add DB to Kitty

---
+++ KittyUI/Models/OurDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace KittyUI.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+
+        public OurDbContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
--- KittyUI/Startup.cs
+++ KittyUI/Startup.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
+
+
 [assembly: OwinStartupAttribute(typeof(KittyUI.Startup))]
+
 namespace KittyUI
+
 {
+
     public partial class Startup
+
     {
         public void Configuration(IAppBuilder app)
+
         {
+
             ConfigureAuth(app);
+
         }
+
     }
+
 }
