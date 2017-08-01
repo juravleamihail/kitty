abf254802a7b5bec800597bb9b0d8041eef13910
CristianBanan <cristian.ursache96@yahoo.com>
2017-08-01 14:50:48 +0300

    123

---
+++ Kitty/DB/LocationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Kitty.Reader
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitty.DB
+{
+    class LocationDbContext : DbContext
+    {
+        public DbSet<Location> locations { get; set; }
+
+        public LocationDbContext():base("DefaultConnection")
+        {
+            JsonLocationReader.Cities();
+        }
+    }
+}
