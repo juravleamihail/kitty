1e57537853ecf7ff14273811bfc9f786aa90b70d
juravleamihail <the_mihail@yahoo.com>
2017-08-04 13:40:52 +0300

    upload file

---
+++ KittyUI/Controllers/UploadFileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KittyUI.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        // GET: UploadFileController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private bool isValidContenyType(string contentType)
+        {
+            return contentType.Equals("file/txt");
+        }
+
+
+        [HttpPost]
+        public ActionResult Process(HttpPostedFileBase Location)
+        {
+            if (isValidContenyType(Location.ContentType))
+            {
+                ViewBag.Error = "It's not a text file";
+                return View("Index");
+            }
+            return View("Succes");
+
+        }
+    }
+}
